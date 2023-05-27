64838476047e357dfd810b96d2b1fa83128dd835
Saad Shamsi <114074040+saad-shamsi@users.noreply.github.com>
2023-05-27 06:02:10 +0500

    created product schema

---
+++ sanity-project/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '81imuxm6',
+    dataset: 'production'
+  }
+})
---
+++ sanity/product.ts
@@ -0,0 +1,12 @@
+export const product = {
+  name: "product",
+  type: "document",
+  title: "Product",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+  ],
+};
